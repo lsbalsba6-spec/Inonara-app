06699090607e7b6437d2dfb7540965bb9482cd35
lsbalsba6-spec <lsbalsba6@gmail.com>
2026-09-13 17:03:24 +0200

    content: activate South Africa completeness lock v30

--- backend/data/country_dossiers/__init__.py
+++ backend/data/country_dossiers/__init__.py
@@ -8,6 +8,7 @@ from .south_africa_completion_v26 import apply_south_africa_v26
 from .south_africa_completion_v27 import apply_south_africa_v27
 from .south_africa_completion_v28 import apply_south_africa_v28
 from .south_africa_completion_v29 import apply_south_africa_v29
+from .south_africa_completion_v30 import apply_south_africa_v30
 
 COUNTRY_DOSSIERS[NAMIBIA_DOSSIER["iso2"]] = NAMIBIA_DOSSIER
 for _iso in ("ZA", "NA", "BW"):
@@ -20,5 +21,6 @@ if "ZA" in COUNTRY_DOSSIERS:
     apply_south_africa_v27(COUNTRY_DOSSIERS["ZA"])
     apply_south_africa_v28(COUNTRY_DOSSIERS["ZA"])
     apply_south_africa_v29(COUNTRY_DOSSIERS["ZA"])
+    apply_south_africa_v30(COUNTRY_DOSSIERS["ZA"])
 
 __all__ = ["COUNTRY_DOSSIERS", "SOUTH_AFRICA_DOSSIER", "NAMIBIA_DOSSIER", "country_dossier_index"]
