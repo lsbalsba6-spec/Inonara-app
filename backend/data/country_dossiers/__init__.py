00cc8972c3aab98a692e21fa342d4828b9981016
lsbalsba6-spec <lsbalsba6@gmail.com>
2026-09-13 09:31:10 +0200

    content: activate South Africa v28 enrichment

--- backend/data/country_dossiers/__init__.py
+++ backend/data/country_dossiers/__init__.py
@@ -6,6 +6,7 @@ from .southern_africa_completion_v24 import apply_completion
 from .south_africa_completion_v25 import apply_south_africa_v25
 from .south_africa_completion_v26 import apply_south_africa_v26
 from .south_africa_completion_v27 import apply_south_africa_v27
+from .south_africa_completion_v28 import apply_south_africa_v28
 
 COUNTRY_DOSSIERS[NAMIBIA_DOSSIER["iso2"]] = NAMIBIA_DOSSIER
 for _iso in ("ZA", "NA", "BW"):
@@ -16,5 +17,6 @@ if "ZA" in COUNTRY_DOSSIERS:
     apply_south_africa_v25(COUNTRY_DOSSIERS["ZA"])
     apply_south_africa_v26(COUNTRY_DOSSIERS["ZA"])
     apply_south_africa_v27(COUNTRY_DOSSIERS["ZA"])
+    apply_south_africa_v28(COUNTRY_DOSSIERS["ZA"])
 
 __all__ = ["COUNTRY_DOSSIERS", "SOUTH_AFRICA_DOSSIER", "NAMIBIA_DOSSIER", "country_dossier_index"]
